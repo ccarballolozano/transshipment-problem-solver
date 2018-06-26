52be5dfeab1dc042ebea11aaed3e78644a5f8e18
ccarballolozano <christcl93@gmail.com>
2018-06-26 10:58:27 +0200

    build for win32

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import sys
 from cx_Freeze import setup, Executable
+import os
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os", "tkinter", "functions", "numpy", "scipy"], "excludes": []}
@@ -9,6 +10,14 @@ build_exe_options = {"packages": ["os", "tkinter", "functions", "numpy", "scipy"
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    # Solve error -- KeyError: 'TCL_LIBRARY'
+    PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+    os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+    os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+    build_exe_options['include_files'] = [
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+        ]
 
 setup(name = "main",
       version = "0.1",
